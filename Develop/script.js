f094dce6e557cdc1f634b944f5c68718b87025bc
Nick Duran <nvduran@gmail.com>
2021-02-07 14:16:46 -0600

    9 oclock fixed

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,17 @@
 let current_time = moment().format("HH:mm");
-//let current_hour = moment().format("HH");
-let current_hour = "13";
+let current_hour = moment().format("HH");
+//let current_hour = "9";
 
 console.log(current_time);
 console.log(current_hour);
 
 var i;
-for (i = 9; i <= 17; i++) {
-     debugger;
+for (i = 10; i <= 17; i++) {
+     //debugger;
      var hourBlock = document.getElementById(i);
-     console.log(hourBlock);
-     console.log(hourBlock.id);
-     console.log(current_hour);
+     // console.log(hourBlock);
+     // console.log(hourBlock.id);
+     // console.log(current_hour);
      if(hourBlock.id < current_hour){
           hourBlock.style.backgroundColor = "red";
      }
@@ -24,14 +24,15 @@ for (i = 9; i <= 17; i++) {
          
 };
 
-// var hour9Block = document.getElementById(9);
-// if(hour9Block < current_hour){
-//      hourBlock.style.backgroundColor = "red";
-// }
-// else if(hourBlock.id === current_hour){
-//      hourBlock.style.backgroundColor = "gray";
-// }
-// else{
-//      hourBlock.style.backgroundColor = "green";
-// };
+//9 oclock specifically with jQuery
+var hourString = current_hour.toString();
+console.log(hourString);
+if("9" > current_hour){
+     $("#9").css("backgroundColor", "red");
+}else if("9" === current_hour){
+     $("#9").css("backgroundColor", "gray");
+}else{
+     $("#9").css("backgroundColor", "green");
+};
+
 
